cf6dca787105bf23218c1883d750811491d0acc7
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-25 05:17:40 +0700

    Database abstraction API. django automatically generated sql statements, if not enough yes we can send raw sql query

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    # get list of movies
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+
+    # filter list
+    # Movie.objects.filter(release_year=1984)
+
+    # # get specific movie
+    # Movie.objects.get(id=1)
+    return HttpResponse(output)
